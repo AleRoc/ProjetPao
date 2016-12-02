49693c4e524dfbef5aac824cbe7d1005c2019f4a
Sylvain Cuomo <zenith1313@gmail.com>
2016-12-02 11:35:44 +0100

    Initial Commit

---
+++ src/main/java/iut/univ_amu/fr/App.java
@@ -0,0 +1,13 @@
+package iut.univ_amu.fr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
