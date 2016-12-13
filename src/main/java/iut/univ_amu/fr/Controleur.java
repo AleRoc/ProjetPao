08fe1b275334d2a3bbc69c2fe925aa1c73da6928
AleRoc <alexis.roche97@gmail.com>
2016-12-13 09:12:29 +0100

    debug

---
+++ src/main/java/iut/univ_amu/fr/Controleur.java
@@ -0,0 +1,18 @@
+package iut.univ_amu.fr;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Roche on 13/12/2016.
+ */
+public class Controleur {
+    private ArrayList<PlaceMarche> listeMarche;
+
+    public void choisirAcheteur(){
+
+    }
+
+    public void validerOffre(){
+
+    }
+}
