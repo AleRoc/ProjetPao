05df5d5641ee2dbd8e22a24113c063f6df865fa3
AleRoc <alexis.roche97@gmail.com>
2016-12-13 09:29:09 +0100

    Refonte des classes : -ChambreFroide -Participant -Personne -Label
    Création de Paysan

--- src/main/java/iut/univ_amu/fr/participants/Participant.java
+++ src/main/java/iut/univ_amu/fr/participants/Participant.java
@@ -5,7 +5,5 @@ package iut.univ_amu.fr.participants;
  */
 public abstract class Participant {
     private String nom;
-
-    public abstract void faireOffreVente();
     public abstract void faireOffreAchat();
 }
--- src/main/java/iut/univ_amu/fr/produit/Label.java
+++ src/main/java/iut/univ_amu/fr/produit/Label.java
@@ -4,5 +4,6 @@ package iut.univ_amu.fr.produit;
  * Created by louis on 06/12/2016.
  */
 public class Label {
+    private String nom;
     //TODO
 }
