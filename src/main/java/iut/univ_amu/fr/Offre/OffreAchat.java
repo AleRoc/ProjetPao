f2dbf7ec00eb7b7f49984c5826c989bffe1fed68
AleRoc <alexis.roche97@gmail.com>
2016-12-13 08:49:12 +0100

    debug

---
+++ src/main/java/iut/univ_amu/fr/Offre/OffreAchat.java
@@ -0,0 +1,27 @@
+package iut.univ_amu.fr.Offre;
+
+import iut.univ_amu.fr.participants.Personne;
+
+import java.util.Date;
+
+/**
+ * Created by Roche on 13/12/2016.
+ */
+public class OffreAchat {
+    private Personne acheteur;
+    private float enchere;
+    private Date date;
+
+    public OffreAchat (Personne acheteur, float enchere){
+        this.acheteur = acheteur;
+        this.enchere = enchere;
+    }
+
+    public Personne getAcheteur() {
+        return acheteur;
+    }
+
+    public float getEnchere() {
+        return enchere;
+    }
+}
--- src/main/java/iut/univ_amu/fr/PlaceMarche.java
+++ src/main/java/iut/univ_amu/fr/PlaceMarche.java
@@ -1,14 +1,43 @@
 package iut.univ_amu.fr;
 
+import iut.univ_amu.fr.Offre.OffreVente;
+
 import java.util.ArrayList;
 
 /**
- * Created by louis on 06/12/2016.
+ * Created by Roche on 13/12/2016.
  */
 public class PlaceMarche {
-    //private ArrayList<OffreVente> listeOffres; TODO classes offre
+    private ArrayList<OffreVente> listeOffres;
+    private ArrayList<OffreVente> listeAttente;
 
-    public void creerOffreVente(){};
-    public void creerOffreAchat(){};
+    public void ajouterOffreVente (OffreVente nouvOffre){
+        int num = 0;
+        if (listeOffres.size()==0)
+        {
+            nouvOffre.setNumero(num);
+        }
+        else
+            for (OffreVente offre : listeOffres)
+            {
+                if(num != offre.getNumero()){
+                    nouvOffre.setNumero(num);
+                    break;
+                }
+                num++;
+            }
+        listeAttente.add(nouvOffre);
+    }
 
+    public void retirerOffreVente (OffreVente offre){
+        listeOffres.remove(listeOffres.indexOf(offre));
+    }
+
+    public ArrayList<OffreVente> getListeOffres() {
+        return listeOffres;
+    }
+
+    public void majMarche (OffreVente offre){
+
+    }
 }
