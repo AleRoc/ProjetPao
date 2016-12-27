366332c0ca0f33bf7733bbd92c8f2ab07e8b3345
K41eb <louis.vinchon@outlook.fr>
2016-12-27 10:56:43 +0100

    Raccourcissement d'appels successifs de fonction inutilement longs

--- src/main/java/iut/univ_amu/fr/participants/Participant.java
+++ src/main/java/iut/univ_amu/fr/participants/Participant.java
@@ -1,8 +1,10 @@
 package iut.univ_amu.fr.participants;
 
 import iut.univ_amu.fr.Services;
+import iut.univ_amu.fr.aminaux.Animal;
 import iut.univ_amu.fr.offre.OffreAchat;
 import iut.univ_amu.fr.offre.OffreVente;
+import iut.univ_amu.fr.produit.ProduitFermier;
 
 import java.util.ArrayList;
 
@@ -13,8 +15,12 @@ import java.util.ArrayList;
 public abstract class Participant {
     protected float argent;
     protected Services services;
-
     protected ArrayList<OffreAchat> offresA = new ArrayList<>();
+
+    protected ArrayList<Animal> mesAnimaux = new ArrayList<>(); //variable test pour le stockage provisoire des animaux et produits
+    protected ArrayList<ProduitFermier> mesProduits = new ArrayList<>(); //idem
+
+    // methodes
 
     public void prelever(float montant){
         argent -= montant;
@@ -30,8 +36,21 @@ public abstract class Participant {
     }
 
     public void retirerOffreAchat(OffreAchat offreA){
-        offreA.getOffreV().retirerOffreAchat(offreA);
-        this.retirerOffreAchat(offreA);
+        offreA.supprimer();
+    }
+
+    // methodes test
+
+    public void acquerirAnimal(ArrayList<Animal> animaux){
+        if (0 < animaux.size())
+            for(Animal a : animaux )
+                mesAnimaux.add(a);
+    }
+
+    public void acquerirProduit(ArrayList<ProduitFermier> produits){
+        if (0 < produits.size())
+            for(ProduitFermier p : produits )
+                mesProduits.add(p);
     }
 
     // getters
--- src/main/java/iut/univ_amu/fr/participants/Personne.java
+++ src/main/java/iut/univ_amu/fr/participants/Personne.java
@@ -1,6 +1,12 @@
 package iut.univ_amu.fr.participants;
 
+import iut.univ_amu.fr.Adherence;
+import iut.univ_amu.fr.PlaceMarche;
+import iut.univ_amu.fr.aminaux.Animal;
 import iut.univ_amu.fr.installations.Installation;
+import iut.univ_amu.fr.offre.OffreAchat;
+import iut.univ_amu.fr.offre.OffreVente;
+import iut.univ_amu.fr.produit.ProduitFermier;
 
 import java.util.ArrayList;
 
@@ -8,14 +14,60 @@ import java.util.ArrayList;
  * Created by louis on 06/12/2016.
  */
 public abstract class Personne extends Participant {
-
+    protected String nom;
     protected String prenom;
     protected int age;
+    protected ArrayList<OffreVente> offresV = new ArrayList<>();
 
-    protected float argent;
+    protected ArrayList<Installation> propriétés = new ArrayList<>();
+    protected ArrayList<Adherence> adherences = new ArrayList<>();
 
-    protected ArrayList<Installation> propriétés;
-    protected ArrayList<CentraleDAchat> adherences;
-    public abstract void faireOffreVente();
-    public abstract void adhererCentrale(CentraleDAchat centrale);
+    // methodes
+
+    public void faireOffreVente(PlaceMarche placeDeMarche, ArrayList<ProduitFermier> produits, int prix){
+        if(0 < produits.size())
+            placeDeMarche.ajouterOffreVente(new OffreVente(this, placeDeMarche, produits, prix));
+    }
+
+    public void retirerOffreVente (OffreVente offreV){
+        offreV.supprimer();
+    }
+
+    public void faireOffreAchat(CentraleDAchat centrale, String libelle, int quantite, boolean qualite, float plafond){ //l'idée est qu'en achetant via une centrale on cherche à faire de l'achat de masse et donc des économies, pas d'enchère donc, seulement un plafond
+        centrale.ajouterOffreAdhérent(new OffreAchat(libelle, quantite, qualite, plafond));
+    }
+
+    public void adhererCentrale(CentraleDAchat centrale, boolean renouvellement, int duree){
+        adherences.add(new Adherence( centrale, this, renouvellement, duree ));
+    }
+
+    public void annulerAdherence ( Adherence a){
+        a.supprimer();
+    }
+
+    // getters
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public ArrayList<OffreVente> getOffresV() {
+        return offresV;
+    }
+
+    public ArrayList<Installation> getPropriétés() {
+        return propriétés;
+    }
+
+    public ArrayList<Adherence> getAdherences() {
+        return adherences;
+    }
 }
