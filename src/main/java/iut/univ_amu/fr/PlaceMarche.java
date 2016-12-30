5372ee6cdb1ae591a486313dece6de7d2782bc08
K41eb <louis.vinchon@outlook.fr>
2016-12-30 17:14:03 +0100

    Mass commentaire façon JavaDoc, j'en peux plus... peut être que le push amrchera cette fois

--- src/main/java/iut/univ_amu/fr/PlaceMarche.java
+++ src/main/java/iut/univ_amu/fr/PlaceMarche.java
@@ -9,26 +9,81 @@ import java.util.Map;
 
 /**
  ** Created by Roche on 13/12/2016.
+ * <b>PlaceMarche est la classe représentant une place d'échange de produits et animaux.</b>
+ * <p>
+ *     Cette classe, est caractérisée par les informations suivantes :
+ *     <ul>
+ *         <li>Une liste des offres de vente affichées sur cette place.</li>
+ *         <li>Une liste des offres de vente soumises à cette place mais pas encore validées par le controleur.</li>
+ *     </ul>
+ * </p>
+ *
+ * @see Controleur
+ * @see OffreVente
+ *
+ * @author Louis VINCHON
+ * @version 1.0
  */
 
 public class PlaceMarche {
+
+    /**
+     * La liste des offres de vente affichées. Cet attribut est modifiable.
+     * <p>
+     *     Il s'agit de offres qui ont été validées par le controleur. Ce sont les seulees offres visibles.
+     * </p>
+     */
     private ArrayList<OffreVente> listeOffres = new ArrayList<>();
+
+    /**
+     * la liste des offres en attente de validation. Elles sont invisibles. Cet attribut est modifiable.
+     */
     private ArrayList<OffreVente> listeAttente = new ArrayList<>();
 
     // methodes
 
+    /**
+     * Ajoute à la liste d'attente les offres soumises par les Personnes.
+     *
+     * @see iut.univ_amu.fr.participants.Personne
+     *
+     * @param nouvOffre
+     */
     public void ajouterOffreVente (OffreVente nouvOffre){ listeAttente.add(nouvOffre); }
 
+    /**
+     * Retire une offre de vente de toutes les listes de cette place.
+     * <p>
+     *     Remarque : l'offre n'est pas supprimée, elle existe toujours.
+     *     Cette methode ne doit être utilisée que dans le cas d'un déplacement de l'offre sur une autre instance de PlaceMarche.
+     * </p>
+     *
+     * @param offre
+     */
     public void retirerOffreVente (OffreVente offre){
         listeAttente.remove(offre);
         listeOffres.remove(offre);
     }
 
+    /**
+     * Déplace une offre vente jusqu'à présent en liste d'attente vers la liste affichée.
+     * @param offre
+     */
     public void majMarche (OffreVente offre){
         listeOffres.add(offre);
         listeAttente.remove(offre);
     }
 
+    /**
+     * Vend les biens proposés dans l'offre de vente indiquée aux offres d'achat renseignées dans la Map "produitsAchetés".
+     *
+     * <p>
+     *     La Map "produitsAchetés" a pour key les offres d'achat sélectionnées par le controleur, et en value la liste des produits à attribuer à chacun des Acheteur.
+     * </p>
+     *
+     * @param offreV
+     * @param produitsAchetés
+     */
     public void vendre (OffreVente offreV, Map<OffreAchat, ArrayList<ProduitFermier>> produitsAchetés){ //pour une offre (produits a,b et c) le controleur aura décidé d'attribuer la vente des produits à tel et tel acheteur (a pour acheteur 1, b pour acheteur 2 et c pour un troisième par exemple)
         for (OffreAchat o : produitsAchetés.keySet() ){
         }
@@ -36,10 +91,20 @@ public class PlaceMarche {
 
     //getters
 
+    /**
+     * Retourne la liste des offres.
+     *
+     * @return la liste des offres validées et affichées.
+     */
     public ArrayList<OffreVente> getListeOffres() {
         return listeOffres;
     }
 
+    /**
+     * Retourne la liste d'attente.
+     *
+     * @return la liste des offres en attente de validation.
+     */
     public ArrayList<OffreVente> getListeAttente() {
         return listeAttente;
     }
--- src/main/java/iut/univ_amu/fr/participants/Participant.java
+++ src/main/java/iut/univ_amu/fr/participants/Participant.java
@@ -1,9 +1,9 @@
 package iut.univ_amu.fr.participants;
 
-import iut.univ_amu.fr.Services;
 import iut.univ_amu.fr.aminaux.Animal;
 import iut.univ_amu.fr.offre.OffreAchat;
 import iut.univ_amu.fr.offre.OffreVente;
+import iut.univ_amu.fr.produit.Label;
 import iut.univ_amu.fr.produit.ProduitFermier;
 
 import java.util.ArrayList;
@@ -11,9 +11,31 @@ import java.util.ArrayList;
 
 /**
  * Created by louis on 06/12/2016.
+ *
+ * <b>Participant est la classe abstraite caractérisant les acteurs sur un marché.</b>
+ * <p>
+ *     Cette classe est caractérisée par les informations suivantes :
+ *     <ul>
+ *         <li>L'argent, qui est la somme d'argent que possède le participant.</li>
+ *         <li>Une liste d'offres d'achat, ce sont toutes les offres d'achat formulées par notre participant et qui sont toujours valides.</li>
+ *     </ul>
+ * </p>
+ *
+ * @see OffreAchat
+ *
+ * @author Louis VINCHON
+ * @version 1.0
  */
 public abstract class Participant {
+
+    /**
+     * La somme d'argent possédée par le participant. Cet attribut est modifiable.
+     */
     protected float argent;
+
+    /**
+     * La liste des offres d'achat formulée par ce participant. Cet attribut est modifiable.
+     */
     protected ArrayList<OffreAchat> offresA = new ArrayList<>();
 
     protected ArrayList<Animal> mesAnimaux = new ArrayList<>(); //variable test pour le stockage provisoire des animaux et produits
@@ -21,22 +43,43 @@ public abstract class Participant {
 
     // methodes
 
-    public void prelever(float montant){
-        argent -= montant;
+    /**
+     * Prélève un certain montant sur l'argent de ce particpant.
+     *
+     * @param montant
+     * @return true si le prélèvement a pu s'effectuer (fonds suffisants), false sinon (le prélèvement n'est alors pas effectué).
+     */
+    public boolean prelever(float montant){
+        if(argent>=montant){
+            argent -= montant;
+            return true;
+        }
+        else
+            return false;
     }
 
+    /**
+     * Crédite un certain montant sur l'argent de ce participant.
+     *
+     * @param montant
+     */
     public void crediter(float montant){
         argent += montant;
     }
 
-    public void faireOffreAchat(OffreVente offreV, int quantite, boolean qualite, float enchere ) {
+    public void faireOffreAchat(OffreVente offreV, int quantite, ArrayList<Label> labels, float enchere ) {
         if (quantite <= offreV.getProduitsVendus().size()) {
-            OffreAchat o = new OffreAchat(offreV, this, quantite, qualite, enchere);
+            OffreAchat o = new OffreAchat(offreV, this, quantite, labels, enchere);
             offreV.ajouterOffreAchat(o);
             offresA.add(o);
         }
     }
 
+    /**
+     * Retire une offre d'achat de la liste d'offres de ce participant.
+     *
+     * @param offreA
+     */
     public void retirerOffreAchat(OffreAchat offreA){
         offreA.supprimer();
     }
@@ -57,10 +100,20 @@ public abstract class Participant {
 
     // getters
 
+    /**
+     * Retourne la liste d'offres d'achat.
+     *
+     * @return la liste des offres d'achat formulées par ce participant.
+     */
     public ArrayList<OffreAchat> getOffresA() {
         return offresA;
     }
 
+    /**
+     * Retourne l'argent.
+     *
+     * @return la somme d'argent que possède actuellement ce particpant.
+     */
     public float getArgent() {
         return argent;
     }
